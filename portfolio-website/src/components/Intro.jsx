0884104305c25ae4d0f9c21575c174d405c1abc4
Dominic Labuguen <105623099+dominiclabuguen@users.noreply.github.com>
2023-02-18 00:07:13 -0800

    ignore

--- portfolio-website/src/components/Intro.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Intro(){
-    return (
-        <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">Hello</div>
-    )
-}
-
-export default Intro;
--- portfolio-website/src/components/Timeline.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Timeline(){
-    return (
-        <div></div>
-    )
-}
-
-export default Timeline;
